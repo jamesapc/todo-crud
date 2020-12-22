82de910cf5eb2c8841b2825d316ae7c169807306
jamesapc <winterapc@gmail.com>
2020-12-22 18:39:24 +0700

    edit style

--- src/pages/TodoListPage.js
+++ src/pages/TodoListPage.js
@@ -1,4 +1,4 @@
-import { Card, Layout, Form, Button, Row, Col } from 'antd'
+import { Card, Layout, Form, Button, Row, Col, Input } from 'antd'
 import React, { useState } from 'react'
 
 const TodoListPage = () => {
@@ -25,6 +25,9 @@ const TodoListPage = () => {
             headStyle={{ padding: '10px', fontWeight: 'bold', fontSize: '20px', borderRadius: '10px 10px 0 0', lineHeight: '23px', backgroundColor: '#455A64', color: 'white' }}
             bodyStyle={{ padding: '10px 0 10px 0', boxShadow: '0px 2px 12px rgba(0, 0, 0, 0.1)', borderRadius: '0 0 10px 10px', backgroundColor: '#90A4AE' }}
           >
+            <Form.Item style={{ textAlign: 'center', margin: '10px 0 10px 0' }}>
+              <Input stlye={{ borderRadius: '10px', height: '30px' }} />
+            </Form.Item>
             <ul className="list-container">
               {lists.map((list, index) => {
                 return (
@@ -32,8 +35,8 @@ const TodoListPage = () => {
                 )
               })}
             </ul>
-              <Form.Item style={{ textAlign: 'center' }}>
-                <Button className="submit-button">Submit</Button>
+              <Form.Item style={{ textAlign: 'center', marginTop: '15px' }}>
+                <Button style={{ height: '30px', width: '80px', fontWeight: 'bold', backgroundColor: '#455A64', color: 'white', border: 'none' }} className="submit-button">Submit</Button>
               </Form.Item>
           </Card>
         </Form>
