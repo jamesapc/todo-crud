44a8ebe9673b30cc3f9ac91f29a5666cfe066419
Apichai Pinyorattanayotee <winterapc@gmail.com>
2020-12-23 16:49:56 +0700

    add todo form

--- src/pages/TodoListPage.js
+++ src/pages/TodoListPage.js
@@ -1,22 +1,37 @@
-import { Card, Layout, Form, Button, Row, Col, Input } from 'antd'
+import { Card, Layout, Form, Button, Input } from 'antd'
 import React, { useState } from 'react'
+import { uid } from 'uid'
 
 const TodoListPage = () => {
+  const [text, setText] = useState('')
   const [lists, setList] = useState([
     {
-      todo: 'Write Code'
+      todo: 'Write Code',
+      id: uid()
     },
     {
-      todo: 'Check email'
+      todo: 'Check email',
+      id: uid()
     },
     {
-      todo: 'Done Jobs'
+      todo: 'Done Jobs',
+      id: uid()
     }
   ])
+
+  const handleSubmit = () => {
+    setText('');
+    setList([...lists, {todo: text, id: uid()}])
+  }
+ 
+  const clearTodo = () => {
+    setList([]);
+  }
+
   return (
     <Layout style={{ height: '100%' }}>
       <Layout.Content>
-        <Form>
+        <Form onFinish={handleSubmit} >
           <Card
             title="Todo List"
             bordered={false}
@@ -26,17 +41,18 @@ const TodoListPage = () => {
             bodyStyle={{ padding: '10px 0 10px 0', boxShadow: '0px 2px 12px rgba(0, 0, 0, 0.1)', borderRadius: '0 0 10px 10px', backgroundColor: '#90A4AE' }}
           >
             <Form.Item style={{ textAlign: 'center', margin: '10px 0 10px 0' }}>
-              <Input stlye={{ borderRadius: '10px', height: '30px' }} />
+              <Input onChange={e => setText(e.target.value)} value={text} stlye={{ borderRadius: '10px', height: '30px' }} />
             </Form.Item>
             <ul className="list-container">
-              {lists.map((list, index) => {
-                return (
-                  <li key={index}>{list.todo}</li>
-                )
-              })}
+              {lists.map((list, index) => (
+                <li key={index}>{list.todo}</li>
+              ))}
             </ul>
               <Form.Item style={{ textAlign: 'center', marginTop: '15px' }}>
-                <Button style={{ height: '30px', width: '80px', fontWeight: 'bold', backgroundColor: '#455A64', color: 'white', border: 'none' }} className="submit-button">Submit</Button>
+                <Button htmlType="submit" style={{ height: '30px', width: '80px', fontWeight: 'bold', backgroundColor: '#455A64', color: 'white', border: 'none' }} className="submit-button">Submit</Button>
+              </Form.Item>
+              <Form.Item style={{ textAlign: 'center', marginTop: '15px' }}>
+                <Button onClick={clearTodo} style={{ height: '30px', width: '80px', fontWeight: 'bold', backgroundColor: 'red', color: 'white', border: 'none' }} className="submit-button">Submit</Button>
               </Form.Item>
           </Card>
         </Form>
