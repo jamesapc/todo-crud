514295bd86a66b4b8efdbe7b9b37feb8f9bc036d
Apichai Pinyorattanayotee <winterapc@gmail.com>
2020-12-24 23:53:11 +0700

    add remove todo

--- src/context/TodoContext.js
+++ src/context/TodoContext.js
@@ -1,20 +1,22 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import firebase from 'firebase'
 
-export const StoreContext = React.createContext()
 
 const firebaseConfig = {
   apiKey: "AIzaSyAQWNmdYVPD_VuX5v6KJRK3KnEM3A_oVs8",
-    authDomain: "react-authen-firebase.firebaseapp.com",
-    databaseURL: "https://react-authen-firebase.firebaseio.com",
-    projectId: "react-authen-firebase",
-    storageBucket: "react-authen-firebase.appspot.com",
-    messagingSenderId: "1045489285569",
-    appId: "1:1045489285569:web:09a87d2505a54611e51740"
+  authDomain: "react-authen-firebase.firebaseapp.com",
+  databaseURL: "https://react-authen-firebase.firebaseio.com",
+  projectId: "react-authen-firebase",
+  storageBucket: "react-authen-firebase.appspot.com",
+  messagingSenderId: "1045489285569",
+  appId: "1:1045489285569:web:09a87d2505a54611e51740"
 }
 
 const app = firebase.initializeApp(firebaseConfig);
 
+const StoreContext = React.createContext({
+  firebaseApp: app
+})
 const contextValue = {
   firebaseApp: app
 }
@@ -27,4 +29,14 @@ const TodoContext = ({ children }) => {
   )
 }
 
+const useAuth = () => {
+  const context = useContext(StoreContext)
+  if (!context) {
+    return alert('cannot use context outside context provider')
+  }
+  return context
+}
 export default TodoContext
+export {
+  useAuth
+}
--- src/pages/TodoListPage.js
+++ src/pages/TodoListPage.js
@@ -1,6 +1,9 @@
 import { Card, Layout, Form, Button, Input } from 'antd'
-import React, { useState } from 'react'
+import { DeleteOutlined } from '@ant-design/icons'
+import React, { useContext, useState } from 'react'
+import { useParams } from 'react-router-dom'
 import { uid } from 'uid'
+import { useAuth } from '../context/TodoContext'
 
 const TodoListPage = () => {
   const [text, setText] = useState('')
@@ -20,12 +23,17 @@ const TodoListPage = () => {
   ])
 
   const handleSubmit = () => {
+    const ref = firebaseApp
     setText('');
     setList([...lists, {todo: text, id: uid()}])
   }
  
   const clearTodo = () => {
     setList([]);
+  }
+
+  const removeTodo = (id) => {
+    setList(lists.filter(item => item.id !== id))
   }
 
   return (
@@ -45,15 +53,19 @@ const TodoListPage = () => {
             </Form.Item>
             <ul className="list-container">
               {lists.map((list, index) => (
-                <li key={index}>{list.todo}</li>
+                <li key={index}>{list.todo} 
+                  <div className="remove-todo"><Button onClick={() => removeTodo(list.id)}><DeleteOutlined /></Button></div>
+                </li>
               ))}
             </ul>
-              <Form.Item style={{ textAlign: 'center', marginTop: '15px' }}>
-                <Button htmlType="submit" style={{ height: '30px', width: '80px', fontWeight: 'bold', backgroundColor: '#455A64', color: 'white', border: 'none' }} className="submit-button">Submit</Button>
-              </Form.Item>
-              <Form.Item style={{ textAlign: 'center', marginTop: '15px' }}>
-                <Button onClick={clearTodo} style={{ height: '30px', width: '80px', fontWeight: 'bold', backgroundColor: 'red', color: 'white', border: 'none' }} className="submit-button">Submit</Button>
-              </Form.Item>
+              <div style={{ display: 'flex', justifyContent: 'center' }}>
+                <Form.Item style={{ textAlign: 'center', marginTop: '15px' }}>
+                  <Button htmlType="submit" style={{ marginRight: '5px', height: '30px', width: '80px', fontWeight: 'bold', backgroundColor: '#455A64', color: 'white', border: 'none' }} className="submit-button">Submit</Button>
+                </Form.Item>
+                <Form.Item style={{ textAlign: 'center', marginTop: '15px' }}>
+                  <Button onClick={clearTodo} style={{ marginLeft: '5px', height: '30px', width: '80px', fontWeight: 'bold', backgroundColor: 'red', color: 'white', border: 'none' }} className="submit-button">Clear</Button>
+                </Form.Item>
+              </div>
           </Card>
         </Form>
       </Layout.Content>
